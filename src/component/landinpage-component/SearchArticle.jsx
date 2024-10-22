763556aafb3f5cc5146c04cef278a961f9c1ee24
Thanapat1502 <psy150201027@gmail.com>
2024-10-22 17:52:43 +0700

    fix: adjust SearchBar icon and DropDown menu

--- src/component/landinpage-component/SearchArticle.jsx
+++ src/component/landinpage-component/SearchArticle.jsx
@@ -3,14 +3,16 @@ import SearchIcon from "../icon/SeachIcon";
 function SeachBar() {
   return (
     <>
-      <input
-        type="text"
-        placeholder="Search"
-        className="border rounded-lg py-3 px-4 lg:w-1/3"
-      />
-      <span className="absolute right-16 top-8 lg:top-5">
-        <SearchIcon />
-      </span>
+      <div className="search-bar-container relative w-full h-12">
+        <input
+          type="text"
+          placeholder="Search"
+          className="border rounded-lg py-3 px-4 w-full lg:w-1/3"
+        />
+        <span className="absolute right-3 top-4 lg:top-5 cursor-pointer">
+          <SearchIcon />
+        </span>
+      </div>
     </>
   );
 }
@@ -18,8 +20,14 @@ function SeachBar() {
 function DropDown() {
   const categories = ["Highlight", "Cat", "Inspiration", "General"];
   return (
-    <div className="dropdown-container">
-      <select name="category" id="category" className="w-full h-12 rounded-lg">
+    <div className="dropdown-container lg:hidden">
+      <label htmlFor="category" className="lg:hidden mb-2">
+        Category
+      </label>
+      <select
+        name="category"
+        id="category"
+        className="w-full h-12 rounded-lg border">
         {categories.map((item, index) => {
           return (
             <option key={index} value={item.toLowerCase()}>
@@ -58,9 +66,6 @@ function SearchArticle() {
         className="search-section-main rounded-2xl flex flex-col gap-4 w-full p-4 bg-[#EFEEEB] 
       lg:flex-row-reverse lg:justify-between">
         <SeachBar />
-        <label htmlFor="category" className="lg:hidden">
-          Category
-        </label>
         <CategoryButton />
         <DropDown />
       </div>
