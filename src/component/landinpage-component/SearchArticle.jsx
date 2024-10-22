dd3dbaf526956150f48d332f1b046dc103f84779
Thanapat1502 <psy150201027@gmail.com>
2024-10-22 17:25:49 +0700

    fix: adjust dropdown menu

--- src/component/landinpage-component/SearchArticle.jsx
+++ src/component/landinpage-component/SearchArticle.jsx
@@ -16,22 +16,35 @@ function SeachBar() {
 }
 
 function DropDown() {
+  const categories = ["Highlight", "Cat", "Inspiration", "General"];
   return (
-    <div className="category-dropdown-main border rounded-lg py-3 px-4 relative lg:flex  lg:items-center">
-      <div className="dropdown-button gap-6 absolute hidden lg:flex ">
-        <button className="font-medium text-base rounded-lg hover:bg-[#DAD6D1] px-5 py-3">
-          Highlight
-        </button>
-        <button className="font-medium text-base rounded-lg hover:bg-[#DAD6D1] px-5 py-3">
-          Cat
-        </button>
-        <button className="font-medium text-base rounded-lg hover:bg-[#DAD6D1] px-5 py-3">
-          Inspiration
-        </button>
-        <button className="font-medium text-base rounded-lg hover:bg-[#DAD6D1] px-5 py-3">
-          Ganeral
-        </button>
-      </div>
+    <div className="dropdown-container">
+      <select name="category" id="category" className="w-full h-12 rounded-lg">
+        {categories.map((item, index) => {
+          return (
+            <option key={index} value={item.toLowerCase()}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+}
+
+function CategoryButton() {
+  const categories = ["Highlight", "Cat", "Inspiration", "General"];
+  return (
+    <div className="category-dropdown-main rounded-lg py-3 px-4 relative hidden lg:flex lg:items-center">
+      {categories.map((item, index) => {
+        return (
+          <button
+            key={index}
+            className="font-medium text-base rounded-lg hover:bg-[#DAD6D1] px-5 py-3">
+            {item}
+          </button>
+        );
+      })}
     </div>
   );
 }
@@ -44,37 +57,12 @@ function SearchArticle() {
       <div
         className="search-section-main rounded-2xl flex flex-col gap-4 w-full p-4 bg-[#EFEEEB] 
       lg:flex-row-reverse lg:justify-between">
-        {/* <input
-          type="text"
-          placeholder="Search"
-          className="border rounded-lg py-3 px-4 lg:w-1/3"
-        />
-        <span className="absolute right-16 top-8 lg:top-5">
-          <SearchIcon />
-        </span> */}
         <SeachBar />
-
         <label htmlFor="category" className="lg:hidden">
           Category
         </label>
+        <CategoryButton />
         <DropDown />
-        {/* 
-        <div className="category-dropdown-main border rounded-lg py-3 px-4 relative lg:flex  lg:items-center">
-          <div className="dropdown-button gap-6 absolute hidden lg:flex ">
-            <button className="font-medium text-base rounded-lg hover:bg-[#DAD6D1] px-5 py-3">
-              Highlight
-            </button>
-            <button className="font-medium text-base rounded-lg hover:bg-[#DAD6D1] px-5 py-3">
-              Cat
-            </button>
-            <button className="font-medium text-base rounded-lg hover:bg-[#DAD6D1] px-5 py-3">
-              Inspiration
-            </button>
-            <button className="font-medium text-base rounded-lg hover:bg-[#DAD6D1] px-5 py-3">
-              Ganeral
-            </button>
-          </div>
-        </div> */}
       </div>
     </nav>
   );
