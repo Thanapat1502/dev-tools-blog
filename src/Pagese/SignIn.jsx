001a887515f444423101040232b25b08fdb11df1
Thanapat1502 <psy150201027@gmail.com>
2025-01-21 19:16:05 +0700

    feat: add state to sing in page

--- src/Pagese/SignIn.jsx
+++ src/Pagese/SignIn.jsx
@@ -1,10 +1,52 @@
 import Navebar from "@/component/landinpage-component/Navebar";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 export default function SignIn() {
   const navigate = useNavigate();
   const navLogin = () => {
     navigate(`/login`);
   };
+  //-----state declaration------
+  const [fullNameInput, setFullNameInput] = useState("");
+  const [userNameInput, setuserNameInput] = useState("");
+  const [emailInput, setemailInput] = useState("");
+  const [passwordInput, setpasswordInput] = useState("");
+  const [formData, setFormData] = useState({
+    fullName: "",
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  //----Input Handler----------------------
+  const hendleFullnameinput = (e) => {
+    setFullNameInput(e.target.value);
+  };
+  const hendleUserNameinput = (e) => {
+    setuserNameInput(e.target.value);
+  };
+  const hendleEmailinput = (e) => {
+    setemailInput(e.target.value);
+  };
+  const hendlePasswordinput = (e) => {
+    setpasswordInput(e.target.value);
+  };
+
+  //---Form handler--------
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({
+      fullName: fullNameInput,
+      userName: userNameInput,
+      email: emailInput,
+      password: passwordInput,
+    });
+
+    console.log(
+      `${formData.fullName} | ${formData.userName} | ${formData.email} | ${formData.password}`
+    );
+  };
+
   return (
     <>
       <Navebar />
@@ -12,7 +54,7 @@ export default function SignIn() {
         <div className="SignUp-body flex flex-col justify-center items-center gap-6 rounded-2xl bg-[#EFEEEB] py-10 px-4 mx-3 h-5/6 mt-10 mb-20">
           <h1 className="font-semibold text-4xl text-[#26231E]">Sign up</h1>
           <form
-            action="post"
+            onSubmit={handleSubmit}
             className=" flex flex-col justify-center items-center gap-6">
             <div className="flex flex-col">
               <label
@@ -24,6 +66,7 @@ export default function SignIn() {
                 type="text"
                 name="fullname"
                 placeholder="Full name"
+                onChange={hendleFullnameinput}
                 className="border py-3 pr-3 pl-4 w-80 h-12 rounded-lg"
               />
             </div>
@@ -38,6 +81,7 @@ export default function SignIn() {
                 type="text"
                 name="username"
                 placeholder="Username"
+                onChange={hendleUserNameinput}
                 className="border py-3 pr-3 pl-4 w-80 h-12 rounded-lg"
               />
             </div>
@@ -52,6 +96,7 @@ export default function SignIn() {
                 type="email"
                 name="email"
                 placeholder="Email"
+                onChange={hendleEmailinput}
                 className="border py-3 pr-3 pl-4 w-80 h-12 rounded-lg"
               />
             </div>
@@ -66,6 +111,7 @@ export default function SignIn() {
                 type="password"
                 name="password"
                 placeholder="Password"
+                onChange={hendlePasswordinput}
                 className="border py-3 pr-3 pl-4 w-80 h-12 rounded-lg"
               />
             </div>
